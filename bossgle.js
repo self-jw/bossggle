c1c9c641ca6c7f25895c832e5ae99802aec385b9
Jolly Walia <jwalia@hammer.local>
2017-04-19 10:03:59 -0700

    Added random number function. Enabled reset board button.

--- bossgle.js
+++ bossgle.js
@@ -1,7 +1,13 @@
 $(document).ready(function () {
 
   var logHTML = '';
+  var totalScore = 0;
+  var totalWords = 0;
 
+  // Set initial random letters.
+  randomLetters();
+
+  // The 16 boxes are in a list.
   $('li').click(function () {
 
     var idElement = document.getElementById(this.id);
@@ -26,4 +32,29 @@ $(document).ready(function () {
     }
   });
 
+  // Reset board.
+  $('#reset').on('click', function () {
+    randomLetters();
+  });
+
 });
+
+function randomLetters() {
+
+  var num;
+  var char;
+
+  for (var i = 1; i < 17; i++) {
+    // Random letter ascii code between 65 and 90 (A-Z).
+    // Max (excluded) = 91, Min (included) = 65.
+    num = Math.floor(Math.random() * (91 - 65)) + 65;
+
+    // Convert ascii number to character.
+    char = String.fromCharCode(num);
+
+    // Change box to new character.
+    var boxID = 'box' + i;
+    document.getElementById(boxID).innerHTML = char;
+  }
+
+}
